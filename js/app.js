8f9003f447972ab3179b08ef8e845ad46716e874
Your Name <you@example.com>
2023-02-11 08:32:21 -0800

    frist commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+function data() {
+    let username = document.getElementById("user").value;
+    let emailadd = document.getElementById("email").vlaue;
+    let pass = document.getElementById("password").value;
+    let date = document.getElementById("db").value;
+    let vaild = true;
+    if (username == "") {
+        alert("palse write your name");
+        vaild = false;
+    }
+    if (emailadd == "") {
+        alert("write your emali");
+        vaild = false;
+    }
+    if (pass == "") {
+        alert("write your password");
+        vaild = false;
+    }
+    if (date == "") {
+        alert("write your date");
+        vaild = false;
+    }
+    return vaild;
+}
